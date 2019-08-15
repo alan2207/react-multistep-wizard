6e337ef5d7fe98204dc64c77086c63f696343649
Alan <alan2207@live.com>
2019-08-15 23:37:36 +0200

    safe externalOverrides methods

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { number, object } from 'prop-types'
+
 export const WizardContext = React.createContext()
 
 export default class Wizard extends React.Component {
@@ -9,30 +10,45 @@ export default class Wizard extends React.Component {
   };
 
   static defaultProps = {
-    startStep: 1
+    startStep: 1,
+    externalOverrides: {}
   };
 
   static getDerivedStateFromProps(props, state) {
     return {
       ...state,
-      ...(props.externalOverrides && { ...props.externalOverrides })
+      currentStep: props.externalOverrides.currentStep || state.currentStep
     }
   }
 
   previous = () => {
     if (this.state.currentStep > 1) {
-      this.setState(({ currentStep }) => ({ currentStep: currentStep - 1 }))
+      if (this.props.externalOverrides.previous) {
+        this.props.externalOverrides.previous()
+      } else {
+        this.setState(({ currentStep }) => ({ currentStep: currentStep - 1 }))
+      }
     }
   };
 
   next = () => {
     if (this.state.currentStep < this.state.totalSteps) {
-      this.setState(({ currentStep }) => ({ currentStep: currentStep + 1 }))
+      if (this.props.externalOverrides.next) {
+        this.props.externalOverrides.next()
+      } else {
+        this.setState(({ currentStep }) => ({ currentStep: currentStep + 1 }))
+      }
     }
   };
 
   jump = position => {
-    this.setState({ currentStep: position })
+    if (position <= this.totalSteps) {
+      if (this.props.externalOverrides.jump) {
+        this.props.externalOverrides.jump(position)
+      } else {
+        this.setState({ currentStep: position })
+      }
+    }
   };
 
   init = steps => {
@@ -40,13 +56,13 @@ export default class Wizard extends React.Component {
   };
 
   state = {
-    currentStep: this.props.startStep,
+    currentStep:
+      this.props.externalOverrides.currentStep || this.props.startStep,
     totalSteps: 0,
     init: this.init,
     previous: this.previous,
     next: this.next,
-    jump: this.jump,
-    ...(this.props.externalOverrides && { ...this.props.externalOverrides })
+    jump: this.jump
   };
 
   render() {
