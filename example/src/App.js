41143248e7b8229ef50f6d144fbeb38fa272d38e
Alan <alan2207@live.com>
2019-12-09 15:27:31 +0100

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,39 +1,61 @@
-import React, { Component } from 'react';
+import React from "react";
 
-import { Wizard, Steps, Step, WithWizard } from 'react-multistep-wizard';
+import { Wizard, Steps, Step, WithWizard } from "react-multistep-wizard";
 
-const steps = [{ step: 1 }, { step: 2 }, { step: 3 }];
+const steps = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const StepComponent = ({ ctx, step }) => {
-  React.useEffect(() => {
-    console.log(step);
-  }, []);
-  return <button onClick={ctx.next}>{step.step}</button>;
+const App = () => {
+  const [step, setStep] = React.useState(1);
+  return (
+    <div
+      style={{
+        margin: "20px",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh"
+      }}
+    >
+      <Wizard
+        externalOverrides={{
+          currentStep: step,
+          jump: p => setStep(p),
+          previous: () => setStep(step => step - 1),
+          next: () => setStep(step => step + 1)
+        }}
+      >
+        <div>
+          <WithWizard>
+            {ctx =>
+              steps.map(step => (
+                <button
+                  style={{ width: "40px", fontSize: "25px" }}
+                  key={step}
+                  onClick={() => ctx.jump(step)}
+                >
+                  {step}
+                </button>
+              ))
+            }
+          </WithWizard>
+        </div>
+        <Steps>
+          {steps.map(step => (
+            <Step key={step}>
+              {ctx => (
+                <div>
+                  <h1 style={{ textAlign: "center" }}>{step}</h1>
+                  <button onClick={ctx.previous}>Previous Step</button>
+                  <button onClick={ctx.next}>Next Step</button>
+                </div>
+              )}
+            </Step>
+          ))}
+        </Steps>
+      </Wizard>
+    </div>
+  );
 };
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Wizard>
-          <WithWizard>{ctx => ctx.currentStep}</WithWizard>
-          <Steps>
-            {/* <Step>{ctx => <StepComponent ctx={ctx}>1</StepComponent>}</Step>
-            <Step>{ctx => <StepComponent ctx={ctx}>2</StepComponent>}</Step>
-            <Step>{ctx => <StepComponent ctx={ctx}>3</StepComponent>}</Step>
-            <Step>{ctx => <StepComponent ctx={ctx}>4</StepComponent>}</Step> */}
-            {steps.map(step => (
-              <Step key={step.step}>
-                {ctx => (
-                  <div>
-                    <StepComponent ctx={ctx} step={step} />
-                  </div>
-                )}
-              </Step>
-            ))}
-          </Steps>
-        </Wizard>
-      </div>
-    );
-  }
-}
+export default App;
